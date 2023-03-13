8f596b0613a6040983cd0efd4d562c9c39de5c76
sersid88 <sersidwork@gmail.com>
2023-03-13 13:36:53 +0300

    Revert "Добавил методы записи/чтения в/из файла."
    This reverts commit a450ddc0fc34448f217e6e85e1c7e783721d3469.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Account account = new Account();
-        /*account.createNewAccount();
-        account.saveNewAccountToFile();*/
-        account.accountLogin();
+        account.createNewAccount();
+        account.saveNewAccountToFile();
+
     }       
 }
